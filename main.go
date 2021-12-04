885c4756b7aa9cc2c503ede9c7a8e35098290f5d
Shaswat Prabhat <spd1238@gmail.com>
2021-12-04 12:51:34 +0530

    Add field level validation for user and value fields.

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 	"log"
 	"net/http"
 )
@@ -9,44 +11,54 @@ import (
 var db = make(map[string]string)
 
 type UserValue struct {
-	User  string `json:"user" binding:"required"`
-	Value string `json:"value" binding:"required"`
+	User  string `json:"user" binding:"required,validateUserAndValue"`
+	Value string `json:"value" binding:"required,validateUserAndValue"`
+}
+
+func validateUserAndValue(fl validator.FieldLevel) bool {
+	if len(fl.Field().String()) < 3 {
+		return false
+	}
+	return true
+}
+
+func getPing(context *gin.Context) {
+	context.String(http.StatusOK, "pong")
+}
+func getUserName(context *gin.Context) {
+	user := context.Params.ByName("name")
+	value, ok := db[user]
+	if ok {
+		context.JSON(http.StatusOK, gin.H{"user": user, "value": value})
+	} else {
+		context.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
+	}
+}
+func setUserName(context *gin.Context) {
+	var userValue UserValue
+	log.Println(context.Request.Body)
+
+	err := context.BindJSON(&userValue)
+	if err != nil {
+		context.String(http.StatusBadRequest, "Bad Request MF!!")
+	} else {
+		db[userValue.User] = userValue.Value
+		context.JSON(http.StatusOK, gin.H{"user": userValue.User, "value": userValue.Value})
+	}
 }
 
 func setupRouter() *gin.Engine {
 	r := gin.Default()
-
-	r.GET("/ping", func(context *gin.Context) {
-		context.String(http.StatusOK, "pong")
-	})
-
-	// Get user value
-	r.GET("/getUser/:name", func(context *gin.Context) {
-		user := context.Params.ByName("name")
-		log.Println(db)
-		value, ok := db[user]
-		if ok {
-			context.JSON(http.StatusOK, gin.H{"user": user, "value": value})
-		} else {
-			context.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
-		}
-	})
-	r.POST("/setUser", func(context *gin.Context) {
-		var userValue UserValue
-		log.Println(context.Request.Body)
-
-		error := context.BindJSON(&userValue)
-		if error != nil {
-			context.String(http.StatusBadRequest, "Bad Request MF!!")
-		} else {
-			db[userValue.User] = userValue.Value
-			context.JSON(http.StatusOK, gin.H{"user": userValue.User, "value": userValue.Value})
-		}
-	})
+	r.GET("/ping", getPing)
+	r.GET("/getUser/:name", getUserName)
+	r.POST("/setUser", setUserName)
 	return r
 }
 
 func main() {
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("validateUserAndValue", validateUserAndValue)
+	}
 	r := setupRouter()
 	r.Run(":8080")
 }
